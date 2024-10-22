4b73e4535740f2e174f628e482bcf575f26756fd
Ewerton Bortolozo Nunes Júnior <ewertonbortolozo2009@gmail.com>
2024-10-21 21:36:37 -0300

    Adcionando rotas
    Para testar o GIT até

--- configuration.py
+++ configuration.py
@@ -1,4 +1,5 @@
 from routes.home import home_route
+from routes.estoque import estoque_route
 from database.database import db
 from database.models.remedios import Remedio
 
@@ -8,6 +9,8 @@ def configure_all(app):
 
 def configure_routes(app):
     app.register_blueprint(home_route)
+    app.register_blueprint(estoque_route, url_prefix='/estoque')
+
 
 def configure_db():
     db.connect()
